d3a9924ddea825fcd712d2833fcc95b1aed1acc8
= <=>
2023-05-28 15:05:41 +0530

    custom validatoin for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	validates :name, :mobile, :email, presence: true
 	validates :email, uniqueness: true
-	validates :mobile, numericality: true #, length: {is: 8}
+	validates :mobile, numericality: true, length: {is: 8}
 end
