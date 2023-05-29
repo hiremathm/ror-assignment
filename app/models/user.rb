ba873d8950dc610c85ad4020774272d20189a10a
= <=>
2023-05-29 12:01:13 +0530

    method 3 is test
    moved to latest commit - method 1 is test
    
    moved to latest commit - method 2 is test

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,15 +15,15 @@ class User < ApplicationRecord
 	end
 
 	def method1
-		return "method 1"
+		return "test method 1"
 	end
 
 	def method2
-		return "method 2"
+		return "test method 2"
 	end
 
 	def method3
-		return "method 3"
+		return "test method 3"
 	end
 
 	def method4
